60960dd7d4034d2a4c69f0a06590eb6c7efde981
sergdenisov <xsergeantx@mail.ru>
2024-12-08 16:32:52 +0300

    Solve task 6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,11 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +20,13 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, String> areasMap = areas.stream().collect(Collectors.toMap(Area::getId, Area::getName));
+    return persons
+        .stream()
+        .flatMap(
+            person -> personAreaIds.get(person.id())
+                .stream().map(areaId -> person.firstName() + " - " + areasMap.get(areaId))
+        )
+        .collect(Collectors.toSet());
   }
 }
