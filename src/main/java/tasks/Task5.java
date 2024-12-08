14fa9282c91e525c6f7275776581a1a3f1274ca8
sergdenisov <xsergeantx@mail.ru>
2024-12-08 16:04:27 +0300

    Solve task 5

--- src/main/java/tasks/Task5.java
+++ src/main/java/tasks/Task5.java
@@ -23,6 +23,6 @@ public class Task5 {
   }
 
   public List<ApiPersonDto> convert(List<Person> persons, Map<Integer, Integer> personAreaIds) {
-    return new ArrayList<>();
+    return persons.stream().map(person -> personConverter.convert(person, personAreaIds.get(person.id()))).toList();
   }
 }
