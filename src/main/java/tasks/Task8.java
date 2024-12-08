c8554597d70490858cc15be6ba782a76dab02105
sergdenisov <xsergeantx@mail.ru>
2024-12-08 18:21:08 +0300

    Solve task 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,8 +4,9 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
-import java.util.Collection;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +22,16 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Map<Integer, Set<Resume>> personsResumes = personService
+        .findResumes(persons.stream().map(Person::id).toList())
+        .stream()
+        .collect(Collectors.groupingBy(Resume::personId, Collectors.toSet()));
+    return persons
+        .stream()
+        .map(person -> new PersonWithResumes(
+            person,
+            Optional.ofNullable(personsResumes.get(person.id())).orElse(Set.of())
+        ))
+        .collect(Collectors.toSet());
   }
 }
